cd678352b965d2349b7ea32461e98b20ae6d90bd
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-09-10 12:44:04 +0200

    Cleanup

--- http.go
+++ http.go
@@ -2,64 +2,12 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"io/ioutil"
 	"net/http"
-	"regexp"
 	"strings"
 	"log"
 )
 
-var templates = template.Must(template.ParseFiles("html/dist/index.html"))
-var validPath = regexp.MustCompile("^/([a-zA-Z0-9]+)$")
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	fmt.Println(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
-}
-
 func HandleRootRequest(w http.ResponseWriter, r * http.Request) {
-	//fmt.Println(r.URL.Path[1:])
-	//http.ServeFile(w, r, r.URL.Path[1:])
-
 	path := r.URL.Path
 	if path == "/" {
 		http.ServeFile(w, r, "html/src/index.html")
@@ -68,11 +16,6 @@ func HandleRootRequest(w http.ResponseWriter, r * http.Request) {
 		path = strings.Join(s, "")
 		http.ServeFile(w, r, path)
 	}
-}
-
-func HandleAssetsRequest(w http.ResponseWriter, r * http.Request) {
-	fmt.Println(r.URL.Path[1:])
-	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
 func HandleApiRequest(w http.ResponseWriter, r * http.Request) {
